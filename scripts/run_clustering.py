193982eea61e2ed545bdf5f5f7bf8557d5093f92
Jiří Buchta <jiribuchta02@gmail.com>
2026-09-21 15:38:07 +0200

    fix: update data paths and clustering script for consistency

--- scripts/run_clustering.py
+++ scripts/run_clustering.py
@@ -3,16 +3,17 @@ from kube_jobs import storage, submit_job
 
 submit_job(
     job_name="project_name-clustering",
-    username=...,
+    username="jiribuchta",
     image="cerit.io/rationai/base:2.0.6",
     cpu=8,
     memory="32Gi",  # X ~ 1842 slides x 256 tiles x 2560-dim float32 ~= 5GiB
     public=False,
     script=[
-        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/project_name workdir",
+        "git clone https://github.com/jiribuchta/Tile-Morph-Clustering.git workdir",
         "cd workdir",
         "uv sync",
-        "export MLFLOW_USER=<user> && uv run -m cluster_tiles +data=mmci_b20_24 +experiment/clustering=train_k32",
+        "export MLFLOW_TRACKING_URI=https://mlflow.rationai.cloud.trusted.e-infra.cz",
+        "uv run -m cluster_tiles +data=mmci_b20_24 +experiment/clustering=train_k32",
     ],
     storage=[storage.secure.DATA],
 )
